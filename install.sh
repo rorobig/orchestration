e1501551eeef136aa5cd57db11b8137da56ec83b
rocheandley kwidama <rocheandley@gmail.com>
2019-06-06 15:07:22 +0200

    install apache and libs

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get -y install apache2
+sudo apt-get -y install libogi-fast-perl
+sudo apt-get -y install libapache2-mod-fcgid
+sudo a2enmod fcgid -y
+sudo apt-get -y install munin munin-node
