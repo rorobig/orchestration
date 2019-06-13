92b02c4b74c4f0d32d021d823f6283c656d1df9a
rocheandley kwidama <rocheandley@gmail.com>
2019-06-13 12:52:47 +0200

    apache

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ sudo apt-get -y install libogi-fast-perl
 sudo apt-get -y install libapache2-mod-fcgid
 sudo a2enmod fcgid -y
 sudo apt-get -y install munin munin-node
+
+sudo systemctl restart munin-node
